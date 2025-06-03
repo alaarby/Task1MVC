31047591caa1dd9b8f8f062cbfdcf48aa7df4751
alaarby <154479331+alaarby@users.noreply.github.com>
2025-06-03 23:40:04 +0300

    Add files via upload

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task1MVC.Data;
+
+namespace Task1MVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        AppDbContext context = new AppDbContext();
+        public IActionResult Index()
+        {
+            var products = context.Products.ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int Id) 
+        {
+            var product = context.Products.Find(Id);
+            return View(product);
+        }
+
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task1MVC.Models;
+
+namespace Task1MVC.Data
+
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Server=.; Database=Task1MVC; TrustServerCertificate=True; Trusted_Connection=True");
+        }
+    }
+}
